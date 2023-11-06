be312d1fafb148b2acab2f32ac837d23e2558132
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-11-06 16:26:57 +0900

    update

--- src/Kmj/KmjDAO.java
+++ src/Kmj/KmjDAO.java
@@ -1,13 +1,70 @@
 package Kmj;
 
-import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-import Main.InterfaceDAO;
-import Main.InterfaceDAO.connector;
-import Main.InterfaceDAO.main;
+import Main.MemberDTO;
+import Main.InterfaceDAO.AboutPlay;
+import Main.InterfaceDAO.Connector;
 
-public class KmjDAO extends connector  {
+public class KmjDAO extends Connector implements AboutPlay  {
 
+	@Override
+	public void printRandomRecipe() {
+	 connect();
+	
+		
+	}
+
+	@Override
+	public void userInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkAnswer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void printCorrect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean printInCorrect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int minusScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printRemain() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameEnd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 	
 	
  
---
+++ src/Main/CookDTO.java
@@ -0,0 +1,9 @@
+package Main;
+
+import java.util.ArrayList;
+
+public class CookDTO {
+	private String cookName , createdDate, createdBy;
+	private int cookNum, score;
+	private ArrayList<RecipeDTO> recipeList;
+}
