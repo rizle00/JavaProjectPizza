9fb2488da7a54e713cd70ddb196305e9402e4c0a
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-11-06 09:39:51 +0900

    start

---
+++ src/Main/DTO.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class DTO {
+
+	private String id, pw, nickName, gender, birthday, createdDay, address;
+//	public class memberDTO{
+//		
+//	}
+//	
+//	public class recipeDTO{ DAO에서 배열 생성
+//		private String name;
+//	}
+	
+}
