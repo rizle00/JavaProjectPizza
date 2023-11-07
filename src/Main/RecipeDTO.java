f59d2eb90907e3cea1106d7968589521dd7618eb
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-11-07 18:03:21 +0900

    aaa

--- src/Main/RecipeDTO.java
+++ src/Main/RecipeDTO.java
@@ -4,8 +4,6 @@ public class RecipeDTO {
 	private int recipeNo, cookNum; // 레시피 번호 , 
 	private String ingredient; // 재료
 	
-	private int recipeNo, cookNum;
-	private String ingredient;
 	public RecipeDTO(int recipeNo, int cookNum, String ingredient) {
 		this.recipeNo = recipeNo;
 		this.cookNum = cookNum;
@@ -28,22 +26,7 @@ public class RecipeDTO {
 		this.cookNum = cookNum;
 	}
 
-	public String getIngredient() {
-		return ingredient;
-	}
-
-	public int getRecipeNo() {
-		return recipeNo;
-	}
-	public void setRecipeNo(int recipeNo) {
-		this.recipeNo = recipeNo;
-	}
-	public int getCookNum() {
-		return cookNum;
-	}
-	public void setCookNum(int cookNum) {
-		this.cookNum = cookNum;
-	}
+	
 	public String getIngredient() {
 		return ingredient;
 	}
