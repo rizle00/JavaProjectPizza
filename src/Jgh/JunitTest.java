62fe386c603eb45c2989ec0b85c577f73752cb9b
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2023-11-07 13:19:26 +0900

    dfs
    fds

--- src/Jgh/JunitTest.java
+++ src/Jgh/JunitTest.java
@@ -7,27 +7,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JunitTest {
-
-	@Disabled
-	@DisplayName("로그인 or 종료 선택 ")
-	@Test
-	void test() {
-		Jgh.JghDAO dao = new JghDAO();
-		dao.idao.startGame();
-	}
-	
+//
+//	@Disabled
+//	@DisplayName("로그인 or 종료 선택 ")
+//	@Test
+//	void test() {
+//		Jgh.JghDAO dao = new JghDAO();
+//		dao.idao.startGame();
+//	}
+//	
 	@Disabled
 	@DisplayName("메뉴 선택")
 	@Test
 	void selectTest() {
 		JghDAO dao = new JghDAO();
-		dao.idao.selectMode();
+		dao.selectMode();
 		
-	}
-	
+		}
+//	
+	@Disabled
 	@DisplayName("점수판")
 	@Test
 	void printScore() {
-		printScore();
+		JghDAO dao = new JghDAO();
+		dao.printScore();
 	}
 }
--- src/Main/DAO.java
+++ src/Main/DAO.java
@@ -1,5 +1,12 @@
 package Main;
 
-public class DAO {
+import Jgh.JghDAO;
 
+public class DAO {
+	public static void main(String[] args) {
+		JghDAO jghDAO = new JghDAO();
+		
+		jghDAO.select();
+
+	}
 }
